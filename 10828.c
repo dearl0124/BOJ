a415778eea83df5316134b9302c8ca2438068c18
dearl0124 <83412532+dearl0124@users.noreply.github.com>
2021-06-05 17:15:48 +0900

    10828번
    문제
    
    정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
    
    명령은 총 다섯 가지이다.
    
    push X: 정수 X를 스택에 넣는 연산이다.
    pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
    size: 스택에 들어있는 정수의 개수를 출력한다.
    empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
    top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.

--- 10828.c
+++ 10828.c
@@ -1,1 +1,73 @@
+#include <stdio.h>
+#include <string.h>
 
+int s1[10001];
+int a = 0;
+void PUSH(int x)
+{
+    scanf("%d", &x);
+    s1[a] = x;
+    a++;
+} 
+void POP(int temp)
+{
+    if (a != 0)
+    {
+        a--;
+        printf("%d\n", s1[a]);
+        s1[a] = 0;
+    }
+    else
+        printf("-1\n");
+    
+}
+void SIZE()
+{
+    if (a != 0)
+        printf("%d\n", a);
+    else
+        printf("0\n");
+}
+void EMPTY()
+{
+    if (a != 0)
+        printf("0\n");
+    else
+        printf("1\n");
+}
+void TOP()
+{
+    if (a != 0)
+        printf("%d\n", s1[a - 1]);
+    else
+        printf("-1\n");
+}
+
+int main()
+{
+    int n = 0, x = 0, temp = 0;
+    char input[10] = { 0, };
+    char* Push = "push";
+    char* Pop = "pop";
+    char* Size = "size";
+    char* Empty = "empty";
+    char* Top = "top";
+    scanf("%d\n", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%s", &input);
+
+        if (strcmp(Push, input) == 0)
+            PUSH(x);
+        else if (strcmp(Pop, input) == 0)
+            POP(temp);
+        else if (strcmp(Size, input) == 0)
+            SIZE();
+        else if (strcmp(Empty, input) == 0)
+            EMPTY();
+        else if (strcmp(Top, input) == 0)
+            TOP();
+    }
+    return 0;
+}
