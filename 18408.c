6878fe539e8ade3b3aff3b61729fda9f854d4936
dearl0124 <83412532+dearl0124@users.noreply.github.com>
2021-06-05 14:07:48 +0900

    Create 18408.c

---
+++ 18408.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, count1 = 0, count2 = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        scanf("%d", &x);
+        if (x == 1)
+            count1++;
+        else if (x == 2)
+            count2++;
+    }
+    if (count1 > count2)
+        printf("1\n");
+    else
+        printf("2\n");
+    return 0;
+}
